2436962ffc6d519b7fc17c67999eae1abb1dda9c
solgamd <solgamd@gmail.com>
2019-03-31 21:06:36 +0200

    Recommitting after strange local errors in Terminal

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    let header = document.createElement('header-container');
+
+    let h1 = document.createElement('h1');
+    let hOneText = document.createTextNode("This is an h1");
+    h1.className = 'h1';
+    
+    h1.appendChild(hOneText);
+    document.body.appendChild(h1);
+    document.body.appendChild(header);
+
+    let h2 = document.createElement('h2');
+    let hTwoText = document.createTextNode('This is an h2');
+    h2.className = 'h2';
+
+    h2.appendChild(hTwoText);
+    document.body.appendChild(h2);
+
+    let h3 = document.createElement('h3');
+    let hThreeText = document.createTextNode("This is an h3");
+    h3.className = 'h3';
+
+    h3.appendChild(hThreeText);
+    document.body.appendChild(h3);
+
+    let h4 = document.createElement('h4');
+    let hFourText = document.createTextNode("This is an h4");
+    h4.className = 'h4';
+
+    h4.appendChild(hFourText);
+    document.body.appendChild(h4);
+
+    let h5 = document.createElement('h5');
+    let hFiveText = document.createTextNode("This is an h5");
+    h5.className = 'h5';
+
+    h5.appendChild(hFiveText);
+    document.body.appendChild(h5);
+
+    let h6 = document.createElement('h6');
+    let hSixText = document.createTextNode("This is an h6");
+    h6.className = 'h6';
+
+    h6.appendChild(hSixText);
+    document.body.appendChild(h6);
+
+    window.addEventListener("click", hClicked()); 
+    
+    function hClicked() {
+        let colors = ['blue', 'green', 'teal'];
+        let randomColor = colors[Math.floor(Math.random() * colors.length)];
+        for (i = 0; i < colors.length; i++) {
+            h1.style.color = randomColor;
+        }
+            
+    }
+
+
+})
+
+
